cd1c8d27c94515636cf38ac352ef592d0a97d880
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2021-08-07 12:46:50 +0530

    add db connection

---
+++ src/lk/pirate/demoFx/db/DBConnection.java
@@ -0,0 +1,35 @@
+/*
+ *@author : Lakshika Madhushan <lakshikamadhushan3@gmail.com>
+ *@since : 08/07/2021
+ */
+
+package lk.pirate.demoFx.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection dBConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demoFx", "root", "1234");
+    }
+
+
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
+        return (null==dBConnection)?
+                (dBConnection= new DBConnection()):
+                (dBConnection);
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
+
+
+
+
+
